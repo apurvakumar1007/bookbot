4f9cd8047b84d3ce8e78ae5356a8250e6fc4f636
apurvakumar1007 <apurvakumar2014@gmail.com>
2023-03-14 14:47:43 +0530

    attempt new update

---
+++ main.py
@@ -0,0 +1,30 @@
+with open("books/frankenstein.txt") as f:
+    file_contents = f.read()
+    file_modified = file_contents.lower()
+    words = file_modified.split()
+    word_dict = {} 
+    l_char ={}
+    for word in words:
+        if word in word_dict:
+            word_dict[word] += 1
+        else:
+            word_dict[word] = 1
+    K = max(zip(word_dict.values(),word_dict.keys()))[1]
+    for l in file_modified:
+        if l.isalpha():
+            if l in l_char:
+              l_char[l]+=1
+            else:
+             l_char[l]=1
+    sort_list = []
+    for l_ch in l_char:
+        sort_list.append({"char": l_ch, "num": l_char[l_ch]})
+   # sort_list.sort(reverse=True)
+        
+    print(sort_list)
+
+
+
+
+
+  
